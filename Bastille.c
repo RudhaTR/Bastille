c7bcc0e9d59c415b2c241d881212bbc4d396841f
RudhaTR <2201ai06_anirudh@iitp.ac.in>
2024-04-17 19:19:59 +0530

    Added The new game and along with it added the resepctive helper functions

---
+++ Bastille.c
@@ -0,0 +1,148 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* Memory */
+#define DDR_BASE 0x00000000
+#define DDR_END 0x3FFFFFFF
+#define A9_ONCHIP_BASE 0xFFFF0000
+#define A9_ONCHIP_END 0xFFFFFFFF
+#define SDRAM_BASE 0xC0000000
+#define SDRAM_END 0xC3FFFFFF
+#define FPGA_ONCHIP_BASE 0xC8000000
+#define FPGA_ONCHIP_END 0xC803FFFF
+#define FPGA_CHAR_BASE 0xC9000000
+#define FPGA_CHAR_END 0xC9001FFF
+
+/* Cyclone V FPGA devices */
+#define LEDR_BASE 0xFF200000
+#define HEX3_HEX0_BASE 0xFF200020
+#define HEX5_HEX4_BASE 0xFF200030
+#define SW_BASE 0xFF200040
+#define KEY_BASE 0xFF200050
+#define JP1_BASE 0xFF200060
+#define JP2_BASE 0xFF200070
+#define PS2_BASE 0xFF200100
+#define PS2_DUAL_BASE 0xFF200108
+#define JTAG_UART_BASE 0xFF201000
+#define JTAG_UART_2_BASE 0xFF201008
+#define IrDA_BASE 0xFF201020
+#define TIMER_BASE 0xFF202000
+#define AV_CONFIG_BASE 0xFF203000
+#define PIXEL_BUF_CTRL_BASE 0xFF203020
+#define CHAR_BUF_CTRL_BASE 0xFF203030
+#define AUDIO_BASE 0xFF203040
+#define VIDEO_IN_BASE 0xFF203060
+#define ADC_BASE 0xFF204000
+
+/* Cyclone V HPS devices */
+#define HPS_GPIO1_BASE 0xFF709000
+#define HPS_TIMER0_BASE 0xFFC08000
+#define HPS_TIMER1_BASE 0xFFC09000
+#define HPS_TIMER2_BASE 0xFFD00000
+#define HPS_TIMER3_BASE 0xFFD01000
+#define FPGA_BRIDGE 0xFFD0501C
+
+#define JTAG_UART_BASE ((volatile int*) 0xFF201000)
+#define JTAG_UART_CONTROL ((volatile int*) (0xFF201000+4))
+
+#define yellow 0xff00
+#define white 0xffff
+#define red 0xf800
+#define green 0x07e0
+#define gray 0x8410
+#define purple 0xf81f
+#define black 0x0000
+#define Orange 0xFD20//Works
+#define Blue 0x001F//works
+#define Indigo 0x4810//Dark Blue
+#define Cyan 0x07FF//works
+#define Magenta 0xF81F//works(light purple)
+#define Lime 0x07E0//light green
+#define Teal 0x0410//works
+#define Brown 0xA145//works
+#define gold_orange 0xFE00//works
+#define olive 0x8400//works
+#define coral_pink 0xFBEA//works
+#define peach 0xFDA0//works
+
+int x_centres[9] = {110,160,210,110,160,210,110,160,210}; // centres of the respective squares
+int y_centres[9] = {70,70,70,120,120,120,170,170,170};
+ char seg7[10] =	{0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110, 
+						 0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01100111};//for displaying the calues on HEX_screen
+
+void write_pixel(int x, int y, short colour) 
+{
+  volatile short *vga_addr=(volatile short*)(0x08000000 + (y<<10) + (x<<1));
+  *vga_addr=colour;
+}
+
+short read_pixel(int x, int y) 
+{
+  volatile short *vga_addr=(volatile short*)(0x08000000 + (y<<10) + (x<<1));
+  return *vga_addr;
+}
+
+/* write a single character to the character buffer at x,y
+ * x in [0,79], y in [0,59]
+ */
+void write_char(int x, int y, char c) 
+{
+  // VGA character buffer
+  volatile char * character_buffer = (char *) (0x09000000 + (y<<7) + x);
+  *character_buffer = c;
+}
+void write_string(int x,int y, char s[])
+{
+    int a=x; int b=y;
+    for(int i=0;i<strlen(s);i++)
+    {
+        write_char(a,b,s[i]);
+    a++;
+}
+}
+
+void clear_pixel() {
+  int x, y;
+  for (x = 0; x < 320; x++) {
+    for (y = 0; y < 240; y++) {
+    write_pixel(x,y,0);
+  }
+  }
+}
+void clear_char() {
+  int x, y;
+  for (x = 0; x < 80; x++) {
+    for (y = 0; y < 60; y++) {
+    write_char(x,y,(char)0);
+  }
+  }
+  
+}
+
+/* use write_pixel to set entire screen to black (does not clear the character buffer) */
+void clear_screen() 
+{
+  int x, y;
+  for (x = 0; x < 320; x++) {
+    for (y = 0; y < 240; y++) {
+    write_pixel(x,y,0);
+  }
+  }
+	
+  for (x = 0; x < 80; x++) 
+  {
+    for (y = 0; y < 60; y++) {
+    write_char(x,y,(char)0);
+  }
+  }
+}
+
+char get_jtag(volatile int *JTAG_UART_ptr) 
+{
+  int data;
+  data = *(JTAG_UART_ptr);
+  if (data & 0x00008000) // check INVALID
+    return ((char)data & 0xFF);
+  else
+    return ('\0');
+}

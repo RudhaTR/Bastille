fdeac21ca4a8aa0fa13cb8a740093f4f9950a026
RudhaTR <2201ai06_anirudh@iitp.ac.in>
2024-04-18 18:30:45 +0530

    Removed useless defines

--- Bastille.c
+++ Bastille.c
@@ -1,46 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
 
-/* Memory */
-#define DDR_BASE 0x00000000
-#define DDR_END 0x3FFFFFFF
-#define A9_ONCHIP_BASE 0xFFFF0000
-#define A9_ONCHIP_END 0xFFFFFFFF
-#define SDRAM_BASE 0xC0000000
-#define SDRAM_END 0xC3FFFFFF
-#define FPGA_ONCHIP_BASE 0xC8000000
-#define FPGA_ONCHIP_END 0xC803FFFF
-#define FPGA_CHAR_BASE 0xC9000000
-#define FPGA_CHAR_END 0xC9001FFF
-
 /* Cyclone V FPGA devices */
 #define LEDR_BASE 0xFF200000
 #define HEX3_HEX0_BASE 0xFF200020
 #define HEX5_HEX4_BASE 0xFF200030
 #define SW_BASE 0xFF200040
 #define KEY_BASE 0xFF200050
-#define JP1_BASE 0xFF200060
-#define JP2_BASE 0xFF200070
-#define PS2_BASE 0xFF200100
-#define PS2_DUAL_BASE 0xFF200108
 #define JTAG_UART_BASE 0xFF201000
 #define JTAG_UART_2_BASE 0xFF201008
 #define IrDA_BASE 0xFF201020
 #define TIMER_BASE 0xFF202000
-#define AV_CONFIG_BASE 0xFF203000
-#define PIXEL_BUF_CTRL_BASE 0xFF203020
-#define CHAR_BUF_CTRL_BASE 0xFF203030
-#define AUDIO_BASE 0xFF203040
-#define VIDEO_IN_BASE 0xFF203060
-#define ADC_BASE 0xFF204000
 
-/* Cyclone V HPS devices */
-#define HPS_GPIO1_BASE 0xFF709000
-#define HPS_TIMER0_BASE 0xFFC08000
-#define HPS_TIMER1_BASE 0xFFC09000
-#define HPS_TIMER2_BASE 0xFFD00000
-#define HPS_TIMER3_BASE 0xFFD01000
-#define FPGA_BRIDGE 0xFFD0501C
+
 
 #define JTAG_UART_BASE ((volatile int*) 0xFF201000)
 #define JTAG_UART_CONTROL ((volatile int*) (0xFF201000+4))
@@ -64,11 +36,6 @@
 #define olive 0x8400//works
 #define coral_pink 0xFBEA//works
 #define peach 0xFDA0//works
-
-int x_centres[9] = {110,160,210,110,160,210,110,160,210}; // centres of the respective squares
-int y_centres[9] = {70,70,70,120,120,120,170,170,170};
- char seg7[10] =	{0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110, 
-						 0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01100111};//for displaying the calues on HEX_screen
 
 void write_pixel(int x, int y, short colour) 
 {
